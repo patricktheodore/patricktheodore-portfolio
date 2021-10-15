515657d6e11d18c242721576f254e27ad95b31e5
patricktheodore <patricktheodoresara@gmail.com>
2021-10-15 22:02:46 +0800

    minor qol changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,15 @@ const projectsLinkBtn = $('#see-my-projects-btn');
 projectsLinkBtn.on('click', function(event) {
     event.preventDefault(); 
     document.location = './portfolio.html';
-})
+});
 
+const githubBtn = $('#github-modal-btn');
+const deployedAppModalBtn = $('#deployed-modal-btn'); 
+
+githubBtn.on('click', function() {
+    window.open('https://github.com/patricktheodore/football-highlights-dashboard', '_blank')
+});
+
+deployedAppModalBtn.on('click', function() {
+    window.open('https://patricktheodore.github.io/football-highlights-dashboard/', '_blank')
+});
