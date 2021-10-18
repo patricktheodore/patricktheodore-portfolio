8a01a57618b5a382057a24fab00548b02e259c30
patricktheodore <patricktheodoresara@gmail.com>
2021-10-18 18:21:06 +0800

    contact form functionality

---
+++ assets/js/contactme.js
@@ -0,0 +1,96 @@
+// entry.137476477: nammeee
+// entry.1456617042: emaill
+// entry.1337743317: commenntttt
+// dlut: 1634540961476
+// fvv: 1
+// draftResponse: [null,null,"-124436678167771972"]
+// pageHistory: 0
+// fbzx: -124436678167771972
+
+// Request URL: https://docs.google.com/forms/u/0/d/e/1FAIpQLSfaif8aM506w6fEVixhOcXswHaVZoiZiq1UCZIqLYwTeuWqpA/formResponse
+
+$('#email-form').on('submit', submitHandler); 
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    // grab email
+    const email = $("#emailInput").val()
+
+    // grab name
+    const name = $("#nameInput").val()
+    // grab query
+    const textarea = $("#textareaInput").val();
+
+    // validation regex for email. 
+    if (!validateEmail(email)) {
+        window.alert('please enter a valid email addres'); //sweet alert library (bootstrap dialogues, modals?)
+        return 
+    }
+
+    if (name === '') {
+        window.alert('please enter your name');
+        return
+    }
+
+    if (textarea === '') {
+        window.alert('please enter a comment');
+        return
+    }
+
+    resetForm();
+
+    const payload = {
+        'entry.1456617042': email,
+        'entry.137476477': name,
+        'entry.1337743317': textarea,
+    }
+    const formdata = new FormData();
+
+    for (const key in payload) {
+        
+        formdata.append(key, payload[key])
+        
+    }
+
+    // send request
+    fetch('https://docs.google.com/forms/u/0/d/e/1FAIpQLSfaif8aM506w6fEVixhOcXswHaVZoiZiq1UCZIqLYwTeuWqpA/formResponse', {
+        method: 'POST',
+        headers: {
+            origin: "https://docs.google.com",
+            referer: "https://docs.google.com/forms/d/e/1FAIpQLSfaif8aM506w6fEVixhOcXswHaVZoiZiq1UCZIqLYwTeuWqpA/viewform",
+        },
+        body: formdata,
+    }).then((response) => {
+        if (response.status === 200){
+            // suceess
+            // clear the fields
+            email.val('');
+            name.val('');
+            textarea.val('');
+        }else{
+            // failure
+        }
+    })
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function resetForm() {
+    $('#emailInput').val('');
+    $('#nameInput').val('');
+    $('#textareaInput').val('');
+
+    swal({
+        title: "Message Sent!",
+        text: "Thank you!",
+        icon: "success",
+        button: "Aww yiss!",
+      });
+}
+
+
+//google recaptcha
